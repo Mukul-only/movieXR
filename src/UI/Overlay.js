8615ee4406dd8db4400a6d886765331ca0fd37df
mukul <mukul.kushwahaa@gmail.com>
2023-06-19 19:32:45 +0530

    ...

--- src/UI/Overlay.js
+++ src/UI/Overlay.js
@@ -51,16 +51,16 @@ const Modal = (props) => {
       classNames={{ enterActive: "enter", exitActive: "exit" }}
     >
       <div
-        className="absolute inset-0 flex items-center justify-center bg-transparent-black z-20"
+        className="fixed inset-0 flex items-center justify-center bg-transparent-black z-20 py-10 px-4 md:py-12 md:px-24 xl:py-28 xl:px-52 "
         onClick={() => props.onClick(false)}
       >
         <div
-          className="relative pb-16  rounded-xl bg-background border-2 border-Gray w-[92vw] h-[92vh] md:w-[86vw] md:h[86vh] xl:h-[70vh] xl:w-[60vw] modal overflow-y-auto"
+          className="relative pb-16  rounded-xl bg-background border-2 border-Gray h-full w-full  modal overflow-y-auto"
           onClick={(e) => {
             e.stopPropagation();
           }}
         >
-          <span className="flex justify-between space-x-4 items-center sticky top-0 left-0 right-0 z-10 bg-background py-5 px-8 lg:px-16 border-b border-Dark-700">
+          <span className="flex justify-between space-x-4 items-center sticky top-0 left-0 right-0 z-10 bg-background py-5 px-6 lg:px-16 border-b border-Dark-700">
             <h1 className="text-xl md:text-2xl font-black  rounded-lg border-l-8 border-primary pl-4 md:pl-6 py-2">
               Edit Movie
             </h1>
@@ -70,7 +70,7 @@ const Modal = (props) => {
               onClick={() => props.onClick(false)}
             />
           </span>
-          <div className="px-8 lg:pl-16 w-full lg:w-[60%]">
+          <div className="px-6 lg:pl-16 w-full lg:w-[60%]">
             {form}
             <p
               className="select-none text-gray-400 hover:text-white text-sm font-semibold cursor-pointer py-4 w-max"
