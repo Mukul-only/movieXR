e221a33c898fcb5adebdfcfe31c32022f80af477
mukul <mukul.kushwahaa@gmail.com>
2023-07-02 22:33:13 +0530

    footer added with t&C privacy policy

--- src/UI/BlogPage.js
+++ src/UI/BlogPage.js
@@ -1,6 +1,9 @@
 import Card from "./Card";
-
+import { useEffect } from "react";
 const BlogPage = (props) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <Card className="flex flex-col items-center pt-6 pb-24 md:items-start">
       <h1 className="text-2xl font-bold md:text-3xl">{props.heading}</h1>
